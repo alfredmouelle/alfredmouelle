a23ba2b492510b8f8a4e0a17ccb3739f931e8ba1
Alfred MOUELLE <alfredmouelle@gmail.com>
2024-09-26 12:17:45 +0100

    static error messages no more needed

--- src/schemas/contact.schema.ts
+++ src/schemas/contact.schema.ts
@@ -2,11 +2,11 @@ import { z } from 'zod';
 
 export const contactSchema = z
   .object({
-    name: z.string().min(2, 'Le nom doit contenir au moins 2 caractères'),
-    email: z.string().email('Adresse email invalide'),
+    name: z.string().min(2),
+    email: z.string().email(),
     message: z
       .string()
-      .min(10, 'Le message doit contenir au moins 10 caractères'),
+      .min(10),
   })
   .required();
 
