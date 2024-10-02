92da1f9dd16f44f5e186f352fa9c3fbfed8bc0f7
Alfred MOUELLE <alfredmouelle@gmail.com>
2024-10-02 23:41:51 +0100

    feat: added gradient to brand name

--- app/[locale]/(home)/section-hero.tsx
+++ app/[locale]/(home)/section-hero.tsx
@@ -21,7 +21,7 @@ export const SectionHero = async () => {
       <div className="hero grid min-h-[calc(100vh-20rem)] items-center gap-4 md:grid-cols-3">
         <div className="hero-brand relative order-2 flex flex-col gap-y-2 text-center md:order-none md:col-span-2 md:text-left">
           <p className="text-muted-foreground">{t('greetings')}</p>
-          <h1 className="text-3xl font-bold text-primary">Alfred Mouelle</h1>
+          <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-[#9c60d7] from-primary via-primary">Alfred Mouelle</h1>
 
           <div className="flex items-center justify-center gap-x-0.5 md:justify-start bg-card/10 border shadow-lg w-fit px-4 rounded-sm mx-auto md:mx-0">
             <span className="text-xl font-medium">{'<'}</span>
